204252108804d37d6c22c63d30a5040a45ff133c
Heryakos Meles <110915633+Heryakos@users.noreply.github.com>
2025-06-04 09:25:12 +0300

    Update SimpleServerModel.tsx
    Fix build issues for Vercel

--- src/components/3d/SimpleServerModel.tsx
+++ src/components/3d/SimpleServerModel.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useRef } from "react"
-import { useFrame } from "@react-three/drei"
+import { useFrame } from '@react-three/fiber';
 import type * as THREE from "three"
 
 interface SimpleServerModelProps {
